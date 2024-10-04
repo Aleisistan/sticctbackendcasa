be238d6bfce77bb5ba345d9f7a01604687a42646
Alejandro Toloza <carlosalejandrotoloza@gmail.com>
2024-10-04 12:19:00 -0300

    con puertos 5432 configurado correctos

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ import { UsersModule } from './users/users.module';
     TypeOrmModule.forRoot({
       type: 'postgres',
      host: 'localhost',
-     port: 2425,
+     port: 5432,
      username: 'postgres',
      password: 'secret123!',
      database: 'sticct',
