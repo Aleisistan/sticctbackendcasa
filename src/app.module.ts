79e4df7101e02d0d6f3ef847d59733a79df9423d
Alejandro Toloza <carlosalejandrotoloza@gmail.com>
2024-10-04 12:04:25 -0300

    a ver ahora sin modificar json

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from './orders/entities/order.entity';
 import { OrdersModule } from './orders/orders.module';
@@ -8,13 +9,15 @@ import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,}), // Hace que las variables de entorno estén disponibles globalmente
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 2425,
-      username: 'postgres',
-      password: 'secret123!',
-      database: 'sticct',
+     host: 'localhost',
+     port: 2425,
+     username: 'postgres',
+     password: 'secret123!',
+     database: 'sticct',
       entities: [Order, User],
       synchronize: true,
     }),
