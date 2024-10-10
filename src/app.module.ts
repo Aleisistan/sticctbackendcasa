5cac12b95a95d2086bd6bd88d5d69932fbca830d
Alejandro Toloza <carlosalejandrotoloza@gmail.com>
2024-10-10 10:55:40 -0300

    reapardo render

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -5,7 +6,6 @@ import { Order } from './orders/entities/order.entity';
 import { OrdersModule } from './orders/orders.module';
 import { User } from './users/entities/user.entity';
 import { UsersModule } from './users/users.module';
-
 @Module({
   imports: [
     TypeOrmModule.forRoot({
