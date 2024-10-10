838fcc82cedc38c211d6b5e9063d475971b0501a
Alejandro Toloza <carlosalejandrotoloza@gmail.com>
2024-10-10 19:06:39 -0300

    todo sa

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,25 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from './orders/entities/order.entity';
 import { OrdersModule } from './orders/orders.module';
 import { User } from './users/entities/user.entity';
 import { UsersModule } from './users/users.module';
 @Module({
-  imports: [
+  imports: [ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 2425,
-    username: 'postgres',
-    password: 'secret123!',
-    database: 'sticct',
-    //ssl: process.env.TYPEORM_SSL === 'true',
-    entities: [Order, User],
-    synchronize: true,
+      type: process.env.DB_TYPE as 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      ssl: process.env.TYPEORM_SSL === 'true',
+      synchronize: true,
+      entities: [Order, User],
+    
     }),
 UsersModule, OrdersModule],
   controllers: [],
