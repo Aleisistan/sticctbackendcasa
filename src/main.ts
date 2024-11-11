dd1156bca50a5c2e5868f5a3d34f7cd45dcf3c72
Alejandro Toloza <carlosalejandrotoloza@gmail.com>
2024-11-11 15:25:49 -0300

    con cors sorregidos

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://sticct.netlify.app/home', // Cambia por la URL de tu sitio en Netlify
+    origin: 'https://sticct.netlify.app/', // Cambia por la URL de tu sitio en Netlify
     methods: 'GET,POST,PUT,DELETE',                    // Métodos permitidos
     credentials: true                                  // Permitir el envío de cookies o autenticación
   });
-  //await app.listen(3000);
+  await app.listen(3000);
 }
 bootstrap();
