36970c9b44aaa173a314481a09d38b118f72b34c
Alejandro Toloza <carlosalejandrotoloza@gmail.com>
2024-11-11 15:32:08 -0300

    a ver ahora

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://sticct.netlify.app/', // Cambia por la URL de tu sitio en Netlify
+    origin: 'https://sticct.netlify.app', // Cambia por la URL de tu sitio en Netlify
     methods: 'GET,POST,PUT,DELETE',                    // Métodos permitidos
     credentials: true                                  // Permitir el envío de cookies o autenticación
   });
